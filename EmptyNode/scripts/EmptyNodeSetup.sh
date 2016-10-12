b4abd2198677a5a66c746de106dc90e232ac6322
travismc1 <travismc@microsoft.com>
2016-10-12 15:16:08 -0700

    Update empty edgenode script location:

--- EmptyNode/scripts/EmptyNodeSetup.sh
+++ EmptyNode/scripts/EmptyNodeSetup.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
+# The source file of this script is located at https://raw.githubusercontent.com/hdinsight/Iaas-Applications/master/EmptyNode/scripts/EmptyNodeSetup.sh
+# This file is copied at http://hdiconfigactions.blob.core.windows.net/emptyedgenodev01/EmptyNodeSetup.sh for greater reliability
+# To HDInsight Developers: If this is being updated, please update both locations
 echo "Empty node setup"
