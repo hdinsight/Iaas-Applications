0f442f5eb0efde7a7a8a84fc7b6583f958fd3d3f
James Baker <jamesbak@microsoft.com>
2016-11-04 16:22:47 -0700

    Refactored scripts for shared reuse between all Ambari-related installations.

--- TitanDB/scripts/create-ambari-services.sh
+++ TitanDB/scripts/create-ambari-services.sh
@@ -1,67 +1,30 @@
 #!/bin/bash          
 
-# Script run nohup on the active head node only to provision & deploy TitanDB services via Ambari
+# Script run nohup on the head node to provision & deploy TitanDB services via Ambari
+# This script is invoked by calling shared-ambari-installation.sh/launch_detached_install_script - see notes on fixed & variable arguments 
+
+source /tmp/shared-ambari-installation.sh
 
 user=$1
 password=$2
 cluster=$3
-is_active_headnode=$4
-titan_listen_port=${5:-8182}
-num_edge_nodes=${6:-0}
-edgenode_script_tag=${7:-titandb-edgenode-signature-tag}
+active_headnode=$4
+num_edge_nodes=${5:-0}
+edgenode_script_tag=${6:-titandb-edgenode-signature-tag}
+titan_listen_port=${7:-8182}
 selected_topology=${8:-1}
 
 # Determine if AMS collector is running on this node (not necessarily the active headnode)
-ams_collector_host=$(curl -u $user:$password "http://headnodehost:8080/api/v1/clusters/$cluster/services/AMBARI_METRICS/components/METRICS_COLLECTOR?fields=host_components" | jq -r '.host_components[0].HostRoles.host_name')
-
-if [[ $(hostname -f) == $ams_collector_host ]]; then
-    echo "$(date +%T) Restarting AMS to make new whitelist metrics effective"
-    su - ams -c'/usr/sbin/ambari-metrics-collector --config /etc/ambari-metrics-collector/conf/ restart'
-fi
+restart_ams_if_necessary $user $password $cluster
 
 # We only need the service registration to proceed once - do it on the active headnode
-if [[ $is_active_headnode ]]; then
-    # We defer the required reboot of Ambari - to make the TitanDB service effective, until after the entire cluster,
-    # including edge nodes, have been fully deployed. 
-    # To detect when the edge node(s) have been fully deployed, we watch for a request to 'run_customscriptaction' which
-    # is the script action running on the edge nodes
-    echo "$(date +%T) Waiting for the registration of $num_edge_nodes edge nodes"
-    start_time=$(($(date +%s) * 1000))
-    # Wait around for 30 mins
-    timeout_time=$(($(date +%s) + 30 * 60))
-    edge_node_hosts=()
-    while [[ ${#edge_node_hosts[@]} -lt $num_edge_nodes ]]; do
-        edge_node_hosts=()
-        custom_action_request_ids=$(curl -u $user:$password "http://headnodehost:8080/api/v1/clusters/$cluster/requests?fields=Requests/request_status&Requests/request_context=run_customscriptaction&Requests/create_time>$start_time" | jq -r '.items[] | select(.Requests.request_status == "COMPLETED") | .Requests.id')
-        for id in $custom_action_request_ids; do
-            is_edge_node_request=$(curl -u $user:$password "http://headnodehost:8080/api/v1/clusters/$cluster/requests/$id/tasks?fields=Tasks/stdout" | jq '[.items[].Tasks.stdout | contains("'$edgenode_script_tag'") ] | all')
-            if [[ $is_edge_node_request == true ]]; then
-                request_hosts=$(curl -u $user:$password "http://headnodehost:8080/api/v1/clusters/$cluster/requests/$id" | jq -r '.Requests.resource_filters[].hosts[]')
-                for host in $request_hosts; do
-                    edge_node_hosts+=($host)
-                done
-            fi
-        done
-        if [[ $(date +%s) -ge $timeout_time ]]; then
-            echo "$(date +%T) FATAL: Timed out waiting for $num_edge_nodes edge nodes to be registered. Current registered hosts: ${edge_node_hosts[*]}"
-            exit
-        fi
-        if [[ ${#edge_node_hosts[@]} -gt 0 ]]; then
-            echo "$(date +%T) Completed edge node hosts: ${edge_node_hosts[*]}"
-        fi
-        sleep 3s
-    done
+if [[ $active_headnode ]]; then
+    # We defer the required reboot of Ambari - to make the TitanDB service effective, until after the entire cluster
+    wait_for_edge_node_scripts $user $password $cluster $num_edge_nodes $edgenode_script_tag
+    # HDI provisioning is complete - we can restart Ambari now
+    make_ambari_service_effective "TitanDB"
 
-    echo "$(date +%T) Proceeding with registration & installation of TitanDB service + components on active head node"
-    sleep 30s
-    echo "$(date +%T) Restarting Ambari on active namenode to register TitanDB service"
-    ambari-server refresh-stack-hash
-    service ambari-server restart
-    
-    # We have to wait for it to come back up properly 
-    sleep 45s
-
-    echo "$(date +%T) Registering TitanDB service with Ambari"
+    log "Registering TitanDB service with Ambari"
     curl -u $user:$password -H "X-Requested-By:ambari" -X POST -d '{"ServiceInfo":{"service_name":"TITANDB"}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services"
     curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/services/TITANDB/components/TITANDB_SERVER"
     curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/services/TITANDB/components/TITANDB_PROXY"
@@ -103,61 +66,29 @@ if [[ $is_active_headnode ]]; then
     else
         server_hosts=(${edge_node_hosts[@]})
     fi
-    echo "$(date +%T) Installing TitanDB Server component using topology: $selected_topology"
-    primary_server_host=""
-    for host in ${server_hosts[@]}; do
-        echo "$(date +%T) Installing TitanDB Server component on host: $host"
-        curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/hosts/$host/host_components/TITANDB_SERVER"
-        if [[ ! $primary_server_host ]]; then
-            primary_server_host=$host
-        fi
-    done
+    log "Installing TitanDB Server component using topology: $selected_topology"
+    primary_server_host=${server_hosts[0]}
+    install_component_on_hosts $user $password $cluster "TITANDB_SERVER" ${server_hosts[@]}
 
     # deploy proxy component to all edge nodes if selected topology
     if [[ $selected_topology -eq 2 ]]; then
-        echo "$(date +%T) Installing TitanDB proxy component on all edge nodes"
-        for host in ${edge_node_hosts[@]}; do
-            echo "$(date +%T) Installing TitanDB proxy component on host: $host"
-            curl -u $user:$password -H "X-Requested-By:ambari" -X POST "http://headnodehost:8080/api/v1/clusters/$cluster/hosts/$host/host_components/TITANDB_PROXY"
-        done
+        install_component_on_hosts $user $password $cluster "TITANDB_PROXY" ${edge_node_hosts[@]}
     fi
     sleep 5s
 
     # install now & wait for the installation to complete
-    response=$(curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Install TITANDB daemon services"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services/TITANDB")
-    install_id=$(echo $response | jq -r '.Requests.id')
-    if [[ $install_id != null ]]; then
-        echo "$(date +%T) Install request id is: $install_id"
-        percent_complete=0
-        timeout_time=$(($(date +%s) + 5 * 60))
-        while [ $percent_complete -lt 100 ]; do
-            complete=$(curl -u $user:$password -H "X-Requested-By:ambari" "http://headnodehost:8080/api/v1/clusters/$cluster/requests/$install_id" | jq -r '.Requests.progress_percent')
-            percent_complete=$(printf "%.0f" $complete)
-            echo "$(date +%T) Install completion percentage: $percent_complete"
-            if [[ $(date +%s) -ge $timeout_time ]]; then
-                echo "$(date +%T) Timed out waiting for TitanDB components to install."
-                break
-            fi
-            sleep 3s
-        done
+    deploy_ambari_service $user $password $cluster "TITANDB"
+    # Start 1 instance of the TitanDB server initially to avoid potential race condition as mutliple server instances attempt to create the HBase tables concurrently
+    log "Starting primary TitanDB server to create HBase tables..."
+    curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Start TitanDB Server"}, "Body": {"HostRoles": {"state": "STARTED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/hosts/$primary_server_host/host_components/TITANDB_SERVER"
+    sleep 10s
 
-        # Start 1 instance of the TitanDB server initially to avoid potential race condition as mutliple server instances attempt to create the HBase tables concurrently
-        echo "$(date +%T) Starting primary TitanDB server to create HBase tables..."
-        curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Start TitanDB Server"}, "Body": {"HostRoles": {"state": "STARTED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/hosts/$primary_server_host/host_components/TITANDB_SERVER"
-        sleep 10s
-
-        # finally, start the service
-        echo "$(date +%T) Starting the TitanDB service on all hosts"
-        curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Start TITANDB services"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services/TITANDB"
-    else
-
-        echo "$(date +%T) FATAL: Installation of TitanDB components failed. Details: $response"
-    fi
+    # finally, start the service
+    log "Starting the TitanDB service on all hosts"
+    curl -u $user:$password -H "X-Requested-By:ambari" -X PUT -d '{"RequestInfo": {"context":"Start TITANDB services"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' "http://headnodehost:8080/api/v1/clusters/$cluster/services/TITANDB"
 else
 
     # Restart Ambari to cause our new service artifacts to be registered
-    echo "$(date +%T) Restarting Ambari on standby namenode to register TitanDB service"
-    ambari-server refresh-stack-hash
-    service ambari-server restart
+    make_ambari_service_effective "TitanDB"
 fi
-echo "$(date +%T) Completed secondary installation of TitanDB service"
+log "Completed asynchronous installation of TitanDB service"
--- TitanDB/scripts/install-titandb.sh
+++ TitanDB/scripts/install-titandb.sh
@@ -11,77 +11,30 @@ selected_topology=${7:-1}
 titandb_ambari_svc_tar_file=TITANDB.tar.gz
 titandb_ambari_svc_tar_file_uri=https://github.com/jamesbak/Iaas-Applications/files/570224/$titandb_ambari_svc_tar_file
 detached_script_uri=https://raw.githubusercontent.com/jamesbak/Iaas-Applications/titandb/TitanDB/scripts/create-ambari-services.sh
+shared_lib_script_uri=https://raw.githubusercontent.com/jamesbak/Iaas-Applications/titandb/scripts/shared-ambari-installation.sh
+shared_lib_script_loc=/tmp/shared-ambari-installation.sh
 
-echo "$(date +%T) Starting custom action script for provisioning TitanDB as an Ambari service"
+wget $shared_lib_script_uri -O $shared_lib_script_loc
+chmod +x $shared_lib_script_loc
+source $shared_lib_script_loc
+
+log "Starting custom action script for provisioning TitanDB as an Ambari service"
 apt-get -y install jq
 
-cluster=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters | jq -r .items[0].Clusters.cluster_name)
-stack_name=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters/$cluster/stack_versions | jq -r '.items[0].ClusterStackVersions.stack')
-stack_version=$(curl -u $user:$password http://headnodehost:8080/api/v1/clusters/$cluster/stack_versions | jq -r '.items[0].ClusterStackVersions.version')
-echo "$(date +%T) Cluster: $cluster, Stack: $stack_name-$stack_version" 
-
-cd /var/lib/ambari-server/resources/stacks/$stack_name/$stack_version/services
-wget "$titandb_ambari_svc_tar_file_uri" -O /tmp/$titandb_ambari_svc_tar_file
-tar -xvf /tmp/$titandb_ambari_svc_tar_file
-chmod -R 644 TITANDB
-
-# We have to enable the Ambari agents to pickup the new service artifacts
-sed -i "s/\(agent.auto.cache.update=\).*/\1true/" /etc/ambari-server/conf/ambari.properties
+get_cluster_stack_details $user $password
+install_ambari_service_tarball "TITANDB" $titandb_ambari_svc_tar_file $titandb_ambari_svc_tar_file_uri 
 
 # OPTIONAL - Install IFrame View to point to our edge node so that we get the TitanDB GUI hosted in Ambari Web App
 if [ ! -z "$edgenode_dns_suffix" ]; then
-    edge_uri="https://$cluster-$edgenode_dns_suffix.apps.azurehdinsight.net"
-    echo "$(date +%T) Building IFrame View to point to edge node at address: $edge_uri"
-    mkdir /tmp/titandb-view
-    mkdir /tmp/titandb-view/jar
-    mkdir /tmp/titandb-view/jar/META-INF
-    cd /tmp/titandb-view/jar
-    rm /tmp/titandb-view/titandb-view.jar
-    # The order the we add things to the zip/jar is significant - manifest information must be first
-    echo 'Manifest-Version: 1.0
-Archiver-Version: Plexus Archiver
-Created-By: Apache Maven
-Built-By: root
-Build-Jdk: 1.7.0_111
-
-' > ./META-INF/MANIFEST.MF
-    zip -r /tmp/titandb-view/titandb-view.jar META-INF 
-    echo '
-<html>
-  <body>
-    <iframe src="'$edge_uri'" style="border: 0; position:fixed; top:0; left:0; right:0; bottom:0; width:100%; height:100%">
-  </body>
-</html>
-' > index.html
-    echo '
-<view>
-  <name>TITANDB_VIEW</name>
-  <label>TitanDB View</label>
-  <version>1.0.0</version>
-  <instance>
-    <name>INSTANCE_1</name>
-  </instance>
-</view>
-' > view.xml
-    zip -r /tmp/titandb-view/titandb-view.jar *
-    cp /tmp/titandb-view/titandb-view.jar /var/lib/ambari-server/resources/views
+    create_service_iframe_view $cluster $edgenode_dns_suffix "titandb-view" "TitanDB View" 
 else
-
     num_edge_nodes=0
 fi
 
 # We need to determine if this is the active headnode
-head_ip=$(getent hosts headnodehost | awk '{ print $1; exit }')
-is_active_headnode=$(expr "$(hostname -i)" == "$head_ip")
-echo "$(date +%T) This node is active headnode: $is_active_headnode"
+is_active_headnode
+active_headnode=$?
 
-echo "$(date +%T) Processing service registration on active head node via background script"
-wget "$detached_script_uri" -O /tmp/create-titandb-ambari-services.sh
-chmod 744 /tmp/create-titandb-ambari-services.sh
-mkdir /var/log/titandb
-echo "$(date +%T) Logging background activity to /var/log/titandb/create-ambari-services.out & /var/log/titandb/create-ambari-services.err"
-nohup /tmp/create-titandb-ambari-services.sh $user $password $cluster $is_active_headnode $titan_listen_port $num_edge_nodes $edgenode_script_tag $selected_topology>/var/log/titandb/create-ambari-services.out 2>/var/log/titandb/create-ambari-services.err &
-echo "$(date +%T) TitanDB has been installed as Ambari service. Pending edge node provisioning prior to deployment."
-
-
+launch_detached_install_script $user $password $cluster $active_headnode $num_edge_nodes $edgenode_script_tag $detached_script_uri "create-titandb-ambari-services" "titandb" $titan_listen_port $selected_topology  
+log "TitanDB has been installed as Ambari service. Pending edge node provisioning prior to deployment."
 
