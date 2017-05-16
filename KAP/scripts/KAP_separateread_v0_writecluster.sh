d606bf2484c3ea25a730f0f5764935acb5647200
Jacky Chan <chankongching@gmail.com>
2017-05-17 01:51:14 +0800

    Trouble shooting deployment issue

--- KAP/scripts/KAP_separateread_v0_writecluster.sh
+++ KAP/scripts/KAP_separateread_v0_writecluster.sh
@@ -2,18 +2,25 @@
 export CONTAINERNAME=$1
 export STORAGEACCTNAME=$2
 export ACCOUNTREGION=$3
+
+echo "$@" >> /root/allvar.txt
+
 export KYLINPROPERTIESFILE=`ls /usr/local/kap/kap-*-GA-hbase1.x/conf/kylin.properties`
 
+$BLOBSTOREADDRESS='blob.core.chinacloudapi.cn'
 if [ "$ACCOUNTREGION" = "china" ]; then
-  export $BLOBSTOREADDRESS='blob.core.chinacloudapi.cn'
+  $BLOBSTOREADDRESS='blob.core.chinacloudapi.cn'
 else
-  export $BLOBSTOREADDRESS='blob.core.windows.net'
+  $BLOBSTOREADDRESS='blob.core.windows.net'
 fi
 
-export STORAGESTRING=$STORAGEACCTNAME"."$BLOBSTOREADDRESS
+export STORAGESTRING=$STORAGEACCTNAME'.'$BLOBSTOREADDRESS
 
 # Copy hbase config file
 "/usr/bin/hadoop fs -get wasb://"$CONTAINERNAME"@"$STORAGESTRING"/kylin/hbase-site.xml" $KYLINPROPERTIESFILE
+
+echo "/usr/bin/hadoop fs -get wasb://"$CONTAINERNAME"@"$STORAGESTRING"/kylin/hbase-site.xml" $KYLINPROPERTIESFILE >> /root/allvar.txt
+
 export ZOOKEEPERADDRESS=`awk '/hbase.zookeeper.quorum/{getline; print}' /etc/hbase/*/0/hbase-site.xml | grep -oP '<value>\K.*(?=</value>)'`
 
 sed -i '$ d' $KYLIN_JOB_CONF
@@ -32,10 +39,12 @@ export KYLIN_JOB_CONF_INMEM=`ls /usr/local/kap/kap-*-GA-hbase1.x/conf/kylin_job_
 echo $KYLIN_JOB_CON_SETTINGS >> $KYLIN_JOB_CONF_INMEM
 
 # Setting kylin.server.mode=query
-sed -i 's/kylin.server.mode=.*/kylin.server.mode=query/' $KYLINPROPERTIESFILE
+sed -i 's/kylin.server.mode=.*/kylin.server.mode=all/' $KYLINPROPERTIESFILE
 # Setting kylin.job.scheduler.default=1
 sed -i 's/kylin.job.scheduler.default=.*/kylin.job.scheduler.default=1/' $KYLINPROPERTIESFILE
 # Setting kap.job.helix.zookeeper-address
 sed -i "s/kap.job.helix.zookeeper-address=.*/kap.job.helix.zookeeper-address=$ZOOKEEPERADDRESS/" $KYLINPROPERTIESFILE
 # Setting of cluster-fs
 sed -i "s/.*kylin.storage.hbase.cluster-fs=.*/kylin.storage.hbase.cluster-fs=wasb:\/\/$CONTAINERNAME@$STORAGESTRING/" $KYLINPROPERTIESFILE
+
+echo "kylin.storage.hbase.cluster-fs=wasb:\/\/$CONTAINERNAME@$STORAGESTRING" >> /root/allvar.txt
